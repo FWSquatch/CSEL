cd619c4d60e0dac50456499cd7206ccfd57f864a
FWSquatch <JDavis@eoctech.org>
2017-09-20 09:34:40 -0500

    v 0.31

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 #Merge the config with the code, output it to csel file
 cat csel.cfg payload > /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH
+sed -i "s/#KERNEL#/"`uname -r`"/g" /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH
 chmod 777 /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH #Make it executable
 
 #Check for crontab entry, add it if it doesn't exist
